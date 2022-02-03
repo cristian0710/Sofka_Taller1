a970cf4777a85eb1b49688c160a0e2851b8b886e
Cristian Duran Monsalve <cdm0710@hotmail.com>
2022-02-02 20:43:23 -0500

    Creacion de clase Persona

---
+++ src/Taller1/Main.java
@@ -0,0 +1,9 @@
+package Taller1;
+
+public class Main {
+    public static void main(String[] args){
+        Persona per = new Persona();
+        per.setName("Cristian");
+        System.out.println(per.getName());
+    }
+}
---
+++ src/Taller1/Persona.java
@@ -0,0 +1,31 @@
+package Taller1;
+
+import java.util.Date;
+
+public class Persona {
+    String name;
+    String lastName;
+    String lastName2;
+    Date dateBirth;
+    float height;
+
+    public Persona(String name, String lastName, String lastName2, Date dateBirth, float height) {
+        this.name = name;
+        this.lastName = lastName;
+        this.lastName2 = lastName2;
+        this.dateBirth = dateBirth;
+        this.height = height;
+    }
+
+    public Persona(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
